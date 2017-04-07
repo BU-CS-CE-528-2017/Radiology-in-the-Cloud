8a985b29cf78cd1f6a4f0748018e785fc8e9d577
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-04-07 17:21:30 -0400

    Strip superfluous imports,

--- pfioh/__init__.py
+++ pfioh/__init__.py
@@ -4,5 +4,3 @@ from .crunner import crunner, crunner_ssh
 from .debug   import debug
 from .message import Message
 from .pfioh   import StoreHandler, ThreadedHTTPServer
-from .pman    import pman
-from .purl    import Purl
