0a67a016d17401eaa109b13db87cd7b2f6372794
Emilio Garcia <i.am.emilio@gmail.com>
2017-04-18 21:16:07 -0400

    takes in directories and runs scripts there

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 if [ $1 == "hello-world" ]
-then 
-    python3 helloOpenShift.py
+then
+    if [ -z "$2" ]
+    then 
+        echo "No Directory Specified!"
+    else
+        mv ./helloOpenShift.py $2
+        cd $2
+        python3 helloOpenShift.py
+    fi
 else
     echo "Invalid command!"
 fi
