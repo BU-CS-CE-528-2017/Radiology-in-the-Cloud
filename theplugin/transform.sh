b02647304cb8f901583232aa08bae1da98a69971
Emilio Garcia <i.am.emilio@gmail.com>
2017-04-26 21:21:09 -0400

    all in one dir

--- theplugin/transform.sh
+++ theplugin/transform.sh
@@ -3,11 +3,11 @@
 export workDir="${WORKDIR:-/var/lib/chris}"
 
 if [ -d "$workDir" ]; then
-    mkdir -p $workDir/output
+#    mkdir -p $workDir/output
     for f in $workDir/*
     do
         if [ -f $f ]; then
-            rev $f > $workDir/output/$(basename $f)
+            rev $f > $workDir/$(basename $f).out
         fi
     done
 fi
